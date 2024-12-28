251fbf32f88538eacf0cbbcbb27cb3358b3ee4e5
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2024-12-28 11:02:40 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+(function ($) {
+  "use strict";
+
+ 
+
+
+  // Magnific popup image js
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    },
+  });
+  
+  
+
+  // Mobile menu js start
+  $(".mobile-topbar .bars").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").addClass("active");
+  });
+
+  $(".close-mobile-menu,.mobile-menu-overlay").on("click", function () {
+    $(".mobile-menu-overlay,.mobile-menu-main").removeClass("active");
+  });
+
+  $('.sub-mobile-menu ul').hide();
+  $(".sub-mobile-menu a").on("click", function () {
+    $('.sub-mobile-menu ul').not($(this).next("ul")).slideUp(300);
+    $(".sub-mobile-menu a i").not($(this).find("i")).removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    $(this).next("ul").slideToggle(300);
+    $(this).find("i").toggleClass("fa-chevron-up fa-chevron-down");
+  });
+
+  
+
+  /* Odometer Activeate js */
+  $(document).ready(function () {
+    $('.odometer').appear(function () {
+      var odo = $(".odometer");
+      odo.each(function () {
+        var countNumber = $(this).attr("data-count");
+        $(this).html(countNumber);
+      });
+    });
+  });
+
+  // Testimonial slider js
+  $('.banner-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    // prevArrow: '<i class="fa-solid arrow arrow-prev fa-angle-left"></i>',
+    // nextArrow: '<i class="fa-solid arrow arrow-next fa-angle-right"></i>',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  });
+
+})(jQuery);
